785c8af6591438e281a9160b44a8dbac6a740119
Davide Frattarolo <daddef@gmail.com>
2025-11-18 16:18:07 +0100

    Aggiunto kcc e mod ls group

--- conf.d/myabbrs.fish
+++ conf.d/myabbrs.fish
@@ -6,6 +6,7 @@ abbr -a -- ll 'ezals -TL2'
 abbr -a -- q exit
 abbr -a -- v nvim
 abbr -a -- kc kubectl
+abbr -a -- kcc kubectl ctx
 abbr -a -- zj zellij
 abbr -a -- sysc systemctl
 abbr -a -- syscu systemctl --user
